7b168bb4ff93ef3b14cf575a28705f96ebe4ddc9
redn6gx <bodavis@csumb.edu>
2020-10-22 10:34:35 -0700

    user will now see product clicked on from homepage, user can add products to cart

--- public/javascripts/productPageCode.js
+++ public/javascripts/productPageCode.js
@@ -4,22 +4,18 @@ if (document.readyState == 'loading') {
     ready();
 }
 
-//class for game product to be added into local storage and loaded into cart
-class gameCartProduct{
-    constructor(gName, price, quantity, picture){
-        this.gName = gName;
-        this.price = price;
-        this.quantity = quantity;
-        this.picture = picture;
-    }
-}
+var tempLocalCart = localStorage.getItem("localCart"); 
+var tempLocalQuantityCart = localStorage.getItem("localQuantityCart"); 
+var currentGamePK;
 
-var addedProduct;
-var tempArray = JSON.parse(localStorage.getItem("cartArray"));
-// let tempArray = localStorage.cartArray ? localStorage.getItem("cartArray").split(",") : [];
 
 /*global $*/
 function ready() {
+    // console.log(currentGamePK);
+    const params = new URLSearchParams(window.location.search)
+    console.log(params.get('id'));
+    currentGamePK = params.get('id');
+
     getProduct();
     $('.home_button').on('click', switchToHome);
     $('.cart_button').on('click', switchToCart);
@@ -32,9 +28,9 @@ function getProduct(){
         url: "/productPage/getProduct",
         dataType: "json",
         contentType: "application/json",
-        // data: JSON.stringify({
-        //     productTitle: $(".shop-item-title")[0].innerText,
-        // }),
+        data: JSON.stringify({
+            productPK: currentGamePK,
+        }),
         success: function (data, status) {
             console.log(data);
             console.log(data.success);
@@ -45,6 +41,8 @@ function getProduct(){
                 console.log(data.message);
             }
                 console.log(data.retrievedGame[0].name);
+                console.log(data.retrievedGame[0].gamePK);
+
                 //display game properties on productPage.hbs
                 $('.shop-item-title').html(data.retrievedGame[0].name);
                 $('.shop-item-price').html(data.retrievedGame[0].price);
@@ -57,19 +55,37 @@ function getProduct(){
 
 //Add to Cart Button
 function addToCart() {
-    let tempQuantity = $('.shop-item-quantity').html().split(":", 1);
-    addedProduct = new gameCartProduct( $('.shop-item-title').html(), $('.shop-item-price').html(),
-    tempQuantity[0], $('.shop-item-image').attr('src'));
+    let duplicateFlag = false;
 
-    console.log(addedProduct.gName, addedProduct.price, addedProduct.quantity, addedProduct.picture);
+    if(tempLocalCart == ""){
+        addToLocalStorageCart();
+        return;
+    }
+    //holds primary keys of games in cart
+    let localCartArray = tempLocalCart.split(",");
 
-    tempArray.push(addedProduct);
-    localStorage.setItem("cartArray", JSON.stringify(tempArray));
+    for(let i=0; i<localCartArray.length; i++){
+        if(localCartArray[i] == currentGamePK){
+            duplicateFlag = true;
+            break;
+        }
+    }
 
-    let testArray = JSON.parse(localStorage.getItem("cartArray"));
-    console.log(testArray[0].gName);
+    if(duplicateFlag){
+        console.log('Item already in cart');
+    }else{
+        addToLocalStorageCart();
+    }
 }
-
+ 
+function addToLocalStorageCart(){
+    tempLocalCart += currentGamePK + ',';
+    tempLocalQuantityCart += $('.cart-quantity-input').val()+ ',';
+    localStorage.setItem("localCart", tempLocalCart);
+    localStorage.setItem("localQuantityCart", tempLocalQuantityCart);
+    console.log(localStorage.getItem("localCart"));
+    console.log(localStorage.getItem("localQuantityCart"));
+}
 
 //switch to home page
 function switchToHome() {
